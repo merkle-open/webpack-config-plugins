7397b0eeaff6b419d121d6dc1ea9bbead6e572ba
Jan Biasi <jan.biasi@namics.com>
2018-06-29 15:43:14 +0200

    fix(ts-config-webpack-plugin): remove dist files from tsc

--- packages/ts-config-webpack-plugin/test/fixtures/entry.spec.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-describe('sometest', function () {
-    expect(10).toBe(10);
-});
-//# sourceMappingURL=entry.spec.js.map
--- packages/ts-config-webpack-plugin/test/fixtures/entry.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('sometest', () => {
-	expect(10).toBe(10);
-})
