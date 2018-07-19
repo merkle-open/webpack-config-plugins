346e7b753980561bf6e85fc465346a351cd1480a
Jan Nicklas <j.nicklas@me.com>
2018-07-19 15:23:22 +0200

    refactor(TsConfigWebpackPlugin): Split production and development configuration
    fixes #8

--- packages/ts-config-webpack-plugin/src/TsConfigWebpackPlugin.js
+++ packages/ts-config-webpack-plugin/src/TsConfigWebpackPlugin.js
@@ -9,53 +9,135 @@ const os = require('os');
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+
+/**
+ * Common Development Config
+ *
+ * @param {TsConfigWebpackPluginOptions} options
+ * @returns {any}
+ */
+const developmentConfig = options => ({
+	module: {
+		rules: [
+			{
+				// .ts, .tsx, .d.ts
+				test: /\.(tsx?|d.ts)$/,
+				use: [
+					{
+						// enable file based cache
+						loader: require.resolve('cache-loader'),
+						options: {
+							cacheDirectory: path.resolve(
+								path.dirname(require.resolve('cache-loader')),
+								'../.cache-loader'
+							),
+						}
+					},
+					{
+						// run compilation threaded
+						loader: require.resolve('thread-loader'),
+						options: {
+							// there should be 1 cpu for the fork-ts-checker-webpack-plugin
+							workers: os.cpus().length - 1,
+						},
+					},
+					{
+						// main typescript compilation loader
+						loader: require.resolve('ts-loader'),
+						options: {
+							/**
+							 * Increase build speed by disabling typechecking for the
+							 * main process and is required to be used with thread-loader
+							 * @see https://github.com/TypeStrong/ts-loader/blob/master/examples/thread-loader/webpack.config.js
+							 * Requires to use the ForkTsCheckerWebpack Plugin
+							 */
+							happyPackMode: true,
+							transpileOnly: true,
+							experimentalWatchApi: true,
+						},
+					}
+				],
+			},
+		],
+	},
+	plugins: [
+		// Webpack plugin that runs typescript type checker on a separate process.
+		new ForkTsCheckerWebpackPlugin({
+			// block webpack's emit to wait for type checker/linter and to add errors to the webpack's compilation
+			// also required for the the overlay functionality of webpack-dev-server
+			async: false,
+			// checkSyntacticErrors is required as we use happyPackMode and the thread-loader to parallelise the builds
+			checkSyntacticErrors: true,
+		})
+	],
+});
+
+/**
+ *
+ * Common Production Config
+ *
+ * @param {TsConfigWebpackPluginOptions} options
+ * @returns {any}
+ */
+const productionConfig = options => ({
+	module: {
+		rules: [
+			{
+				// .ts, .tsx, .d.ts
+				test: /\.(tsx?|d.ts)$/,
+				use: [
+					{
+						// enable file based cache
+						loader: require.resolve('cache-loader'),
+						options: {
+							cacheDirectory: path.resolve(
+								path.dirname(require.resolve('cache-loader')),
+								'../.cache-loader'
+							),
+						}
+					},
+					{
+						// run compilation threaded
+						loader: require.resolve('thread-loader'),
+						options: {
+							// there should be 1 cpu for the fork-ts-checker-webpack-plugin
+							workers: os.cpus().length - 1,
+						},
+					},
+					{
+						// main typescript compilation loader
+						loader: require.resolve('ts-loader'),
+						options: {
+							/**
+							 * Increase build speed by disabling typechecking for the
+							 * main process and is required to be used with thread-loader
+							 * @see https://github.com/TypeStrong/ts-loader/blob/master/examples/thread-loader/webpack.config.js
+							 * Requires to use the ForkTsCheckerWebpack Plugin
+							 */
+							happyPackMode: true,
+							transpileOnly: true,
+						},
+					}
+				],
+			},
+		],
+	},
+	plugins: [
+		// Webpack plugin that runs typescript type checker on a separate process.
+		new ForkTsCheckerWebpackPlugin({
+			// checkSyntacticErrors is required as we use happyPackMode and the thread-loader to parallelise the builds
+			checkSyntacticErrors: true,
+		})
+	],
+});
+
+
 class TsConfigWebpackPlugin {
 	/**
 	 * @param {TsConfigWebpackPluginOptions} options
 	 */
 	constructor(options = {}) {
 		this.options = options;
-	}
-
-	getCacheLoader() {
-		return {
-			loader: require.resolve('cache-loader'),
-			options: {
-				cacheDirectory: path.resolve(
-					path.dirname(require.resolve('cache-loader')),
-					'../.cache-loader'
-				),
-			},
-		};
-	}
-
-	getThreadLoader() {
-		return {
-			loader: require.resolve('thread-loader'),
-			options: {
-				// there should be 1 cpu for the fork-ts-checker-webpack-plugin
-				workers: os.cpus().length - 1,
-			},
-		};
-	}
-
-	/**
-	 * @param {boolean} increaseSpeed
-	 */
-	getTsLoader(increaseSpeed = true) {
-		return {
-			loader: require.resolve('ts-loader'),
-			options: {
-				/**
-				 * Increase build speed by disabling typechecking for the
-				 * main process and is required to be used with thread-loader
-				 * @see https://github.com/TypeStrong/ts-loader/blob/master/examples/thread-loader/webpack.config.js
-				 */
-				happyPackMode: true,
-				transpileOnly: increaseSpeed,
-				experimentalWatchApi: increaseSpeed,
-			},
-		};
 	}
 
 	/**
@@ -65,30 +147,18 @@ class TsConfigWebpackPlugin {
 		// From https://github.com/webpack/webpack/blob/3366421f1784c449f415cda5930a8e445086f688/lib/WebpackOptionsDefaulter.js#L12-L14
 		const isProductionLikeMode =
 			compiler.options.mode === 'production' || !compiler.options.mode;
-
-		[
-			new ForkTsCheckerWebpackPlugin({
-				async: false,
-				checkSyntacticErrors: true,
-			}),
-		].forEach(plugin => plugin.apply(compiler));
-
+		const config = isProductionLikeMode
+			? productionConfig(this.options)
+			: developmentConfig(this.options);
+		// Merge config
+		config.plugins.forEach(plugin => plugin.apply(compiler));
 		compiler.hooks.afterEnvironment.tap('TsConfigWebpackPlugin', () => {
-
-			compiler.options.resolve.extensions.push('.ts', '.tsx', '.d.ts');
-
-			compiler.options.module.rules.push({
-				test: /\.(tsx?|d.ts)$/, // ts, tsx, d.ts
-				use: [
-					// enable cache for all inputs
-					this.getCacheLoader(),
-					// run compilation threaded
-					this.getThreadLoader(),
-					// main typescript compilation process
-					this.getTsLoader(!isProductionLikeMode),
-				],
-			});
+			compiler.options.module.rules.push(config.module.rules[0]);
 		});
+		// Prepend missing typescript file extensions
+		const typescriptExtensions = ['.ts', '.tsx', '.d.ts']
+			.filter((ext) => !compiler.options.resolve.extensions.includes(ext));
+		compiler.options.resolve.extensions.unshift(...typescriptExtensions);
 	}
 }
 
