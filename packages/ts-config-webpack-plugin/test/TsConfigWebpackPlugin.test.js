4390594103fb1556781734cb2a2d5b113fd892ce
Jan Nicklas <j.nicklas@me.com>
2018-07-23 13:04:40 +0200

    test(TsConfigWebpackPlugin): Add SourceMap tests
    fix #1

--- packages/ts-config-webpack-plugin/test/TsConfigWebpackPlugin.test.js
+++ packages/ts-config-webpack-plugin/test/TsConfigWebpackPlugin.test.js
@@ -3,6 +3,8 @@ const rimraf = require('rimraf');
 const glob = require('glob');
 const webpack = require('webpack');
 const TsConfigWebpackPlugin = require('../src/TsConfigWebpackPlugin');
+const fs = require('fs');
+const validateSourcemap = require('sourcemap-validator');
 
 beforeAll(done => {
 	rimraf(path.join(__dirname, 'fixtures/dist'), done);
@@ -159,5 +161,115 @@ describe('TsConfigWebpackPlugin inside webpack context', () => {
 		expect(ruleToTest.test.test('test.tsx')).toBe(true);
 		expect(ruleToTest.test.test('testingts')).toBe(false);
 		done();
+	});
+});
+
+describe('TsConfigWebpackPlugin sourcemaps', () => {
+	const extractEval = evalStatement => {
+		const start = evalStatement.indexOf('"') + 1;
+		const end = evalStatement.lastIndexOf('"');
+		return eval(`"${evalStatement.substr(start, end - start)}"`);
+	};
+
+	it('should generate a valid sourcemap in eval mode', done => {
+		/**
+		 * https://webpack.js.org/configuration/devtool/
+		 * eval - Each module is executed with eval() and //@ sourceURL.
+		 * This is pretty fast. The main disadvantage is that it doesn't display line numbers
+		 * correctly since it gets mapped to transpiled code instead of the original code (No Source Maps from Loaders).
+		 */
+		const compiler = webpack({
+			mode: 'development',
+			devtool: 'eval',
+			context: path.join(__dirname, 'fixtures/simple'),
+			plugins: [new TsConfigWebpackPlugin()],
+		});
+		compiler.run((err, stats) => {
+			const distFile = fs.readFileSync(
+				path.resolve(__dirname, 'fixtures/dist/main.js'),
+				'utf8'
+			);
+			const match = distFile.match(/\# sourceURL=([\:\\\/\.a-z0-9]+)(.)/i) || {
+				1: 'No sourcemap found',
+			};
+			expect(match[0]).not.toBeFalsy();
+			expect(match[1]).toBe('webpack:///./src/index.ts');
+			done();
+		});
+	});
+
+	it('should generate a valid sourcemap in eval-source-map mode', done => {
+		/**
+		 * https://webpack.js.org/configuration/devtool/
+		 * eval-source-map - Each module is executed with eval() and a SourceMap is added as a DataUrl to the eval().
+		 * Initially it is slow, but it provides fast rebuild speed and yields real files.
+		 * Line numbers are correctly mapped since it gets mapped to the original code.
+		 * It yields the best quality SourceMaps for development.
+		 */
+		const compiler = webpack({
+			mode: 'development',
+			devtool: 'eval-source-map',
+			context: path.join(__dirname, 'fixtures/simple'),
+			plugins: [new TsConfigWebpackPlugin()],
+		});
+		compiler.run((err, stats) => {
+			const srcFile = fs.readFileSync(
+				path.resolve(__dirname, 'fixtures/simple/src/index.ts'),
+				'utf8'
+			);
+			const distFile = fs.readFileSync(
+				path.resolve(__dirname, 'fixtures/dist/main.js'),
+				'utf8'
+			);
+			// Webpack wraps the code in eval blocks so it has to be extracted before it can be analyzed
+			const evalSourceMap = extractEval(
+				distFile.substr(distFile.indexOf('eval(')).split('\n', 2)[0]
+			);
+			expect(evalSourceMap).not.toBeFalsy();
+			const match = evalSourceMap.match(
+				/\# sourceMappingURL=data\:[^,]+,([\:/a-z0-9+-]+)/i
+			) || { 1: 'No sourcemap found' };
+			expect(match[0]).not.toBeFalsy();
+			const sourceMapString = Buffer.from(match[1], 'base64').toString();
+			validateSourcemap(evalSourceMap, sourceMapString, srcFile);
+			done();
+		});
+	});
+
+	it('should generate a valid sourcemap in cheap-module-eval-source-map mode', done => {
+		/**
+		 * https://webpack.js.org/configuration/devtool/
+		 * cheap-module-eval-source-map - Similar to cheap-eval-source-map, however,
+		 * in this case Source Maps from Loaders are processed for better results.
+		 * However Loader Source Maps are simplified to a single mapping per line.
+		 */
+		const compiler = webpack({
+			mode: 'development',
+			devtool: 'cheap-module-eval-source-map',
+			context: path.join(__dirname, 'fixtures/simple'),
+			plugins: [new TsConfigWebpackPlugin()],
+		});
+		compiler.run((err, stats) => {
+			const srcFile = fs.readFileSync(
+				path.resolve(__dirname, 'fixtures/simple/src/index.ts'),
+				'utf8'
+			);
+			const distFile = fs.readFileSync(
+				path.resolve(__dirname, 'fixtures/dist/main.js'),
+				'utf8'
+			);
+			// Webpack wraps the code in eval blocks so it has to be extracted before it can be analyzed
+			const evalSourceMap = extractEval(
+				distFile.substr(distFile.indexOf('eval(')).split('\n', 2)[0]
+			);
+			expect(evalSourceMap).not.toBeFalsy();
+			const match = evalSourceMap.match(
+				/\# sourceMappingURL=data\:[^,]+,([\:/a-z0-9+-]+)/i
+			) || { 1: 'No sourcemap found' };
+			expect(match[0]).not.toBeFalsy();
+			const sourceMapString = Buffer.from(match[1], 'base64').toString();
+			validateSourcemap(evalSourceMap, sourceMapString, srcFile);
+			done();
+		});
 	});
 });
