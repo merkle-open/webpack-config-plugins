2d677950fbcf842eccd489b949a42c51d2e0186c
Davide De Giorgio <davide.degiorgio@namics.com>
2018-07-28 16:55:56 +0200

    refactor(scss-config-webpack-plugin): move configurations for development and production into own file

---
+++ packages/scss-config-webpack-plugin/src/config/development.config.js
@@ -0,0 +1,105 @@
+const autoprefixer = require('autoprefixer');
+
+/**
+ * Common Development Config
+ *
+ * @param {ScssConfigWebpackPluginOptions} options
+ * @returns {any}
+ */
+exports = module.exports = options => ({
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				exclude: /\.module\.s?css$/,
+				use: [
+					{
+						loader: require.resolve('style-loader'),
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: require.resolve('css-loader'),
+						options: {
+							sourceMap: true,
+							importLoaders: 3,
+						},
+					},
+					{
+						loader: require.resolve('postcss-loader'),
+						options: {
+							plugins: loader => [
+								require('postcss-flexbugs-fixes'),
+								autoprefixer({
+									// flexbox: "no-2009" will add prefixes only for final and IE versions of specification.
+									// @see https://github.com/postcss/autoprefixer#disabling
+									flexbox: 'no-2009',
+								}),
+								require('iconfont-webpack-plugin')({
+									resolve: loader.resolve,
+								}),
+							],
+							sourceMap: true,
+						},
+					},
+					{
+						loader: require.resolve('resolve-url-loader'),
+					},
+					{
+						loader: require.resolve('sass-loader'),
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /module\.s?css$/,
+				use: [
+					{
+						loader: require.resolve('style-loader'),
+						options: {
+							sourceMap: false,
+						},
+					},
+					{
+						loader: require.resolve('css-loader'),
+						options: {
+							sourceMap: false,
+							importLoaders: 3,
+							modules: true,
+						},
+					},
+					{
+						loader: require.resolve('postcss-loader'),
+						options: {
+							plugins: loader => [
+								require('postcss-flexbugs-fixes'),
+								autoprefixer({
+									// flexbox: "no-2009" will add prefixes only for final and IE versions of specification.
+									// @see https://github.com/postcss/autoprefixer#disabling
+									flexbox: 'no-2009',
+								}),
+								require('iconfont-webpack-plugin')({
+									resolve: loader.resolve,
+								}),
+							],
+							sourceMap: true,
+						},
+					},
+					{
+						loader: require.resolve('resolve-url-loader'),
+					},
+					{
+						loader: require.resolve('sass-loader'),
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [],
+});
---
+++ packages/scss-config-webpack-plugin/src/config/production.config.js
@@ -0,0 +1,108 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+/**
+ *
+ * Common Production Config
+ *
+ * @param {ScssConfigWebpackPluginOptions} options
+ * @returns {any}
+ */
+exports = module.exports = options => ({
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				exclude: /\.module\.s?css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: require.resolve('css-loader'),
+						options: {
+							sourceMap: true,
+							importLoaders: 3,
+						},
+					},
+					{
+						loader: require.resolve('postcss-loader'),
+						options: {
+							plugins: loader => [
+								require('postcss-flexbugs-fixes'),
+								autoprefixer({
+									// flexbox: "no-2009" will add prefixes only for final and IE versions of specification.
+									// @see https://github.com/postcss/autoprefixer#disabling
+									flexbox: 'no-2009',
+								}),
+								require('iconfont-webpack-plugin')({
+									resolve: loader.resolve,
+								}),
+							],
+							sourceMap: true,
+						},
+					},
+					{
+						loader: require.resolve('resolve-url-loader'),
+					},
+					{
+						loader: require.resolve('sass-loader'),
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /module\.s?css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: require.resolve('css-loader'),
+						options: {
+							sourceMap: true,
+							importLoaders: 3,
+							modules: true,
+						},
+					},
+					{
+						loader: require.resolve('postcss-loader'),
+						options: {
+							plugins: loader => [
+								require('postcss-flexbugs-fixes'),
+								autoprefixer({
+									// flexbox: "no-2009" will add prefixes only for final and IE versions of specification.
+									// @see https://github.com/postcss/autoprefixer#disabling
+									flexbox: 'no-2009',
+								}),
+								require('iconfont-webpack-plugin')({
+									resolve: loader.resolve,
+								}),
+							],
+							sourceMap: true,
+						},
+					},
+					{
+						loader: require.resolve('resolve-url-loader'),
+					},
+					{
+						loader: require.resolve('sass-loader'),
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].min.css',
+			chunkFilename: '[id].css',
+		}),
+		new OptimizeCSSAssetsPlugin({}),
+	],
+});
