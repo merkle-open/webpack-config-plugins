49ef8c6342831f1945ca357a299931382059ea30
Jan R. Biasi <janbiasi@users.noreply.github.com>
2018-11-28 16:11:18 +0100

    feat(ts-config-webpack-plugin): improve performance by adding more cpu cores for the type-checker

--- packages/ts-config-webpack-plugin/config/development.config.js
+++ packages/ts-config-webpack-plugin/config/development.config.js
@@ -2,6 +2,13 @@ const os = require('os');
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+const cpus = os.cpus().length;
+
+// we use <max> - 2 workers for quadcore cpus and higher, two cpus are reserved
+// for the ts checker plugin (which at least needs one).
+const tsLoaderWorkers = cpus > 3 ? cpus - 2 : 1;
+const forkTsCheckerWorkers = Math.max(1, cpus - tsLoaderWorkers);
+
 /**
  * Common Development Config
  *
@@ -30,7 +37,7 @@ exports = module.exports = (options) => ({
 						loader: require.resolve('thread-loader'),
 						options: {
 							// there should be 1 cpu for the fork-ts-checker-webpack-plugin
-							workers: os.cpus().length - 1,
+							workers: tsLoaderWorkers,
 						},
 					},
 					{
@@ -56,9 +63,10 @@ exports = module.exports = (options) => ({
 	plugins: [
 		// Webpack plugin that runs typescript type checker on a separate process.
 		new ForkTsCheckerWebpackPlugin({
-			// block webpack's emit to wait for type checker/linter and to add errors to the webpack's compilation
-			// also required for the the overlay functionality of webpack-dev-server
-			async: false,
+			// don't block webpack's emit to wait for type checker, errors only visible inside CLI
+			async: true,
+			// increase performance on multicore systems
+			workers: forkTsCheckerWorkers,
 			// checkSyntacticErrors is required as we use happyPackMode and the thread-loader to parallelise the builds
 			checkSyntacticErrors: true,
 			// Set the tsconfig.json path
