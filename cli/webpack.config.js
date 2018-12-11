05867878175a693b7a4750a64b313abd91a9a92f
Jan Nicklas <j.nicklas@me.com>
2018-12-11 15:11:54 +0100

    fix(cli): Add node hashbang

--- cli/webpack.config.js
+++ cli/webpack.config.js
@@ -1,4 +1,5 @@
 const TsConfigWebpackPlugin = require('ts-config-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
 	mode: 'production',
@@ -7,6 +8,12 @@ module.exports = {
 		filename: 'generate-webpack-config.js',
 	},
 	plugins: [
+		// Add the hashbang
+		// inorder to be executable
+		new webpack.BannerPlugin({
+			banner: '#!/usr/bin/env node',
+			raw: true,
+		}),
 		// Multi threading typescript loader configuration with caching for .ts and .tsx files
 		// see https://github.com/namics/webpack-config-plugins/tree/master/packages/ts-config-webpack-plugin/config
 		new TsConfigWebpackPlugin(),
